d9eb42c4fab0bfa13c46725b2b5fbd7f21307674
igor <igoratakhanov@gmail.com>
2019-12-09 13:23:58 -0500

    simple test complete, clicking on quarter div increments quarter, cleanup

--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { render, unmountComponentAtNode } from 'react-dom';
+import { unmountComponentAtNode } from 'react-dom';
 import App from './App';
 import { act } from "react-dom/test-utils";
+import { render, fireEvent, waitForElement } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 
 let container = null;
 describe("App",() => {
   beforeEach(() => {
     container = document.createElement('div');
     document.body.appendChild(container)
-    // ReactDOM.render(<App />, div);
-    // ReactDOM.unmountComponentAtNode(div);
   })
 
   afterEach(() => {
-    // cleanup on exiting
     unmountComponentAtNode(container);
     container.remove();
     container = null;
@@ -23,19 +22,6 @@ describe("App",() => {
     act(() => {
       render(<App />, container);
     });
-    const home = document.querySelector('.home__score')
-    const away = document.querySelector('.away__score')
-    expect(home.textContent).toBe("0");
-    expect(away.textContent).toBe("0");
-  })
-
-  it('adds to home and away score', () => {
-    act(() => {
-      render(<App />, container);
-    });
-
-
-
     const home = document.querySelector('.home__score')
     const away = document.querySelector('.away__score')
     expect(home.textContent).toBe("0");
