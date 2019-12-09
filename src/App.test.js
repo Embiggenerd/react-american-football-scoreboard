8809b83a4f786e19f362784497b365b70fe5ea60
igor <igoratakhanov@gmail.com>
2019-12-09 12:51:52 -0500

    test for default App component complete

--- src/App.test.js
+++ src/App.test.js
@@ -1,25 +1,44 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, unmountComponentAtNode } from 'react-dom';
 import App from './App';
 import { act } from "react-dom/test-utils";
 
 let container = null;
-it('renders without crashing', () => {
-  beforeEach(()=>{
+describe("App",() => {
+  beforeEach(() => {
     container = document.createElement('div');
-    document.appendChild(container)
+    document.body.appendChild(container)
     // ReactDOM.render(<App />, div);
     // ReactDOM.unmountComponentAtNode(div);
   })
 
   afterEach(() => {
     // cleanup on exiting
-    ReactDOM.unmountComponentAtNode(container);
+    unmountComponentAtNode(container);
     container.remove();
     container = null;
   });
 
-  
+  it('renders without crashing', () => {
+    act(() => {
+      render(<App />, container);
+    });
+    const home = document.querySelector('.home__score')
+    const away = document.querySelector('.away__score')
+    expect(home.textContent).toBe("0");
+    expect(away.textContent).toBe("0");
+  })
+
+  it('adds to home and away score', () => {
+    act(() => {
+      render(<App />, container);
+    });
 
 
+
+    const home = document.querySelector('.home__score')
+    const away = document.querySelector('.away__score')
+    expect(home.textContent).toBe("0");
+    expect(away.textContent).toBe("0");
+  })
 });
